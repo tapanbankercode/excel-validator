6830b5723619f6169ee20a4079119f8c1ac732ca
Tapan Banker -Mac <bankertapan@gmail.com>
2018-02-03 13:12:49 -0500

    Credentials files

---
+++ src/main/java/io/database/postgresql/DatabaseCredentials.java
@@ -0,0 +1,18 @@
+package io.database.postgresql;
+
+/**
+ * Created by Tapan Banker on 2/2/18.
+ * @Author Tapan Banker
+ * Interface to store Database credentials
+ */
+public interface DatabaseCredentials {
+    // Database Host URL
+    public static final String HOST = "jdbc:postgresql://localhost:5432/";
+    // Database Name
+    public static final String DB_NAME = "DB_NAME";
+    // Database username
+    public static final String USERNAME = "USERNAME";
+    // Database password
+    public static final String PASSWORD = "PASSWORD";
+
+}
